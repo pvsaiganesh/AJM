76ef25b10f82fadbb1157bbb9e7a8d3d2a07d40b
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-21 12:09:04 +0530

    updated code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import "./Header.scss";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Image from "react-bootstrap/Image";
+import Logo from "../../assets/AJM.svg";
+import { Link } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="container-fluid p-2">
-        <div className="row">
-          <div className="col-4">
+      <Container fluid className="pt-3 pb-3">
+        <Row>
+          <Col xs="4">
             <span className="me-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -18,13 +28,16 @@ const Header = () => {
                 <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
                 <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
               </svg>
+              Location: Vizag, Andhra Pradesh, India
             </span>
-            Location: Vizag, Andhra Pradesh, India
-          </div>
-          <div className="col-4 flex-fill text-orange text-center fw-bold">
+          </Col>
+          <Col
+            xs="4"
+            className="col-4 flex-fill text-orange text-center fw-bold"
+          >
             Get a Free Quote
-          </div>
-          <div className="col-4 text-end ">
+          </Col>
+          <Col xs="4" className="col-4 text-end ">
             <span className="me-2 bg-primary rounded-circle p-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -41,9 +54,39 @@ const Header = () => {
               </svg>
             </span>
             +91 9550482920
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row className="d-flex flex-row justify-content-start align-items-center">
+          <Col xs="3">
+            <Link to="/">
+              <Image className="logo" src={Logo} fluid rounded />
+            </Link>
+          </Col>
+          <Col xs="2">
+            <Autocomplete
+              size="small"
+              renderInput={(params) => (
+                <TextField {...params} label="Search here..." />
+              )}
+            />
+          </Col>
+
+          <Col xs="4">
+            <Navbar bg="white" data-bs-theme="light">
+              <Container>
+                <Nav className="me-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#features">About Us</Nav.Link>
+                  <Nav.Link href="#pricing">Blogs</Nav.Link>
+                  <Nav.Link href="#pricing">Contact Us</Nav.Link>
+                </Nav>
+              </Container>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
       Header
     </header>
   );
