9a94f9406cbe5a72e40b6a09bb9a76b7c9dd7704
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-22 11:00:53 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
