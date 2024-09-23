77d136278ba396f3a546ee3ff4460ec83b39ce68
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-23 09:53:24 +0530

    updated

--- src/components/Features/FeatureCard.jsx
+++ src/components/Features/FeatureCard.jsx
@@ -4,13 +4,13 @@ import Image from "react-bootstrap/esm/Image";
 
 const FeatureCard = (item) => {
   return (
-    <Col lg="3" className="d-flex flex-row">
+    <Col lg="3" className="feature-card d-flex flex-row">
       <div className="pt-5 pe-5 align-self-center">
-        <span className="rounded-circle bg-grey p-4">
-          <Image src={item.icon} />
+        <span className="rounded-circle p-4 feature-image">
+          <Image className="feature-icon" src={item.icon} />
         </span>
       </div>
-      <div className="pt-5 align-self-center">
+      <div className="pt-5 align-self-center feature-text">
         <p className="fs-6 fw-bold mb-2">{item.title}</p>
         <p className="fs-6">{item.description}</p>
       </div>
