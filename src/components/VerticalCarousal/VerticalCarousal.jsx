d1db2de7534118a11e149c5b1dbfc437f5c2d16d
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-27 17:15:22 +0530

    updated

--- src/components/VerticalCarousal/VerticalCarousal.jsx
+++ src/components/VerticalCarousal/VerticalCarousal.jsx
@@ -1,4 +1,4 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import {
   Pagination,
   Navigation,
@@ -9,13 +9,16 @@ import {
 } from "swiper/modules";
 import "swiper/scss";
 import "./VerticalCarousal.scss";
-import React, { useState } from "react";
-import "../../../node_modules/swiper/modules/navigation.scss";
-import "../../../node_modules/swiper/modules/pagination.scss";
+import React, { useRef, useState } from "react";
+import "../../../node_modules/swiper/modules/navigation";
+import "../../../node_modules/swiper/modules/pagination";
 import "../../../node_modules/swiper/modules";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import Image from "react-bootstrap/esm/Image";
+import img1 from "../../assets/small-image.svg";
+import img2 from "../../assets/thumb-image.svg";
 
 // import "swiper/scss";
 // import "swiper/scss/navigation";
@@ -68,28 +71,34 @@ import Col from "react-bootstrap/esm/Col";
 const VerticalCarousal = () => {
   // store controlled swiper instance
   const [controlledSwiper, setControlledSwiper] = useState(null);
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const thumbsSwiperRef = useRef();
+  const [thumbsSwiper, setThumbsSwiper] = useState(thumbsSwiperRef.current);
 
   const thumbsProps = {
     spaceBetween: 10,
     slidesPerView: 4,
     direction: "vertical",
-    loop: true,
-
+    navigation: true,
+    pagination: true,
     // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    // // Navigation arrows
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
 
     // And if we need scrollbar
     scrollbar: {
       el: ".swiper-scrollbar",
+      draggable: true,
+      clickable: true,
+    },
+    mousewheel: {
+      invert: true,
     },
   };
   return (
@@ -97,34 +106,55 @@ const VerticalCarousal = () => {
       <Row>
         <Col lg="2">
           <Swiper
+            ref={thumbsSwiperRef}
             className="thumbs-carousal"
             modules={[Thumbs, Pagination, Navigation, Scrollbar]}
             watchSlidesProgress
-            // onSwiper={setThumbsSwiper}
+            style={{
+              "--swiper-navigation-color": "#000",
+              "--swiper-pagination-color": "#000",
+            }}
+            // onSwiper={(swiper) => {
+            //   setThumbsSwiper(swiper);
+            // }}
             // controller={{ control: controlledSwiper }}
             {...thumbsProps}
           >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide className="slide-main">
+              <Image src={img1} />
+            </SwiperSlide>
+            <SwiperSlide className="slide-main">
+              <Image src={img2} />
+            </SwiperSlide>
+            <SwiperSlide className="slide-main">Slide 4</SwiperSlide>
+            <SwiperSlide className="slide">
+              <Image src={img2} className="thumb-img" />
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <Image src={img1} className="thumb-img" />
+            </SwiperSlide>
+            <SwiperSlide className="slide">Slide 5</SwiperSlide>
+            <SwiperSlide className="slide">Slide 6</SwiperSlide>
+            <SwiperSlide className="slide">Slide 7</SwiperSlide>
+            <SwiperSlide className="slide">Slide 8</SwiperSlide>
           </Swiper>
         </Col>
         <Col lg="5">
           <Swiper
             className="main-carousal"
             modules={[Thumbs, Pagination, Navigation, Scrollbar]}
-            // onSwiper={setControlledSwiper}
+            // onSwiper={(swiper) => {
+            //   setThumbsSwiper(swiper);
+            // }}
             thumbs={{ swiper: thumbsSwiper }}
           >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide className="slide">
+              <Image src={img1} className="thumb-img" />
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <Image src={img2} className="thumb-img" />
+            </SwiperSlide>
+            <SwiperSlide className="slide">Slide 4</SwiperSlide>
           </Swiper>
         </Col>
       </Row>
