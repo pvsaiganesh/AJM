7b3bbf5b3621ef782a7565afbbcb77bd83e3d6fd
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-28 21:01:06 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -86,7 +86,7 @@ const Header = () => {
                   </Link>
                 </Col>
                 <Col className="d-none d-md-block" xs="12" md="3">
-                  <Autocomplete
+                  {/* <Autocomplete
                     freeSolo
                     variant="solid"
                     className="search-box p-2 pe-3 ps-3 rounded"
@@ -105,54 +105,54 @@ const Header = () => {
                         inputProps,
                         ...rest
                       } = params;
-                      return (
-                        <InputBase
-                          ref={InputProps.ref}
-                          {...InputProps}
-                          {...rest}
-                          size="small"
-                          placeholder="Search here..."
-                          startAdornment={
-                            <InputAdornment position="start">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="currentColor"
-                                className="bi bi-search"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                              </svg>
-                            </InputAdornment>
-                          }
-                          endAdornment={
-                            <InputAdornment position="end">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="currentColor"
-                                className="bi bi-list"
-                                viewBox="0 0 16 16"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                                />
-                              </svg>
-                            </InputAdornment>
-                          }
+                      return ( */}
+                  <InputBase
+                    // ref={InputProps.ref}
+                    // {...InputProps}
+                    // {...rest}
+                    size="small"
+                    placeholder="Search here..."
+                    startAdornment={
+                      <InputAdornment position="start">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          className="bi bi-search"
+                          viewBox="0 0 16 16"
                         >
-                          <Input
-                            {...inputProps}
-                            fullWidth
-                            className="search-box-input"
+                          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                        </svg>
+                      </InputAdornment>
+                    }
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          className="bi bi-list"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
                           />
-                        </InputBase>
-                      );
-                    }}
-                  />
+                        </svg>
+                      </InputAdornment>
+                    }
+                  >
+                    <Input
+                      // {...inputProps}
+                      fullWidth
+                      className="search-box-input"
+                    />
+                  </InputBase>
+                  {/* );
+                    }} */}
+                  {/* /> */}
                 </Col>
 
                 <Col className="d-none d-md-block" md="4">
