8da7a6648804c4220e07c7c0e19233b1d05571bc
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-28 13:44:05 +0530

    Update ScrollToTop.js

--- src/hooks/ScrollToTop.js
+++ src/hooks/ScrollToTop.js
@@ -1,21 +1,22 @@
 import { useEffect } from "react";
 import { PropTypes } from "prop-types";
 import withRouter from "./withRouter";
+import { useLocation } from "react-router-dom";
 
-const ScrollToTop = ({ children, location }) => {
+const ScrollToTop = ({ children }) => {
+  const routePath = useLocation();
   useEffect(() => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
-  }, [location]);
+  }, [routePath]);
 
   return children;
 };
 ScrollToTop.propTypes = {
   children: PropTypes.instanceOf(Object),
-  location: PropTypes.instanceOf(Object),
 };
 
 export default withRouter(ScrollToTop);
